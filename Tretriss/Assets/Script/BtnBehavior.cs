92a72482d0cf0df915b24f85b15897d2bc0929cc
Vinciane <qlqqlc@outlook.fr>
2021-10-26 17:17:28 +0200

    Scène Menu et page crédit

---
+++ Tretriss/Assets/Script/BtnBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnBehavior : MonoBehaviour
+{
+    public void commencer()
+    	{
+    		
+    	}
+    	
+    	public void score()
+    	{
+    		
+    	}
+    	
+    	public void quitGame()
+    	{
+    		Debug.Log("QUIT!");
+    		Application.Quit();
+    	}
+    	
+    	public void credit()
+    	{
+    	
+    	}
+}
