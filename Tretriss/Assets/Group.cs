6d24f49a6cb5b9820b7a1a800da1eb5a678fe550
hathas07 <35289971+hathas07@users.noreply.github.com>
2021-10-27 15:44:33 +0200

    Ajout des commandes tactiles

--- Tretriss/Assets/Group.cs
+++ Tretriss/Assets/Group.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class Group : MonoBehaviour
     // Time since last gravity tick
     float lastFall = 0;
 
+    //Touch variable
+    Vector2 initialPosition = Vector2.zero;
+    float touchBeginTime;
+    readonly float swipeMaxDuration = 1f;
+    
     bool isValidGridPos() {        
         foreach (Transform child in transform) {
             Vector2 v = Playfield.roundVec2(child.position);
@@ -50,73 +56,147 @@ public class Group : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Mobile inputs
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            
+            if (touch.phase == TouchPhase.Began)
+            {
+                TouchBegan(touch);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                var touchDuration = Time.time - touchBeginTime;
+
+                if (touchDuration < swipeMaxDuration)
+                {
+                    HandleMove(touch);
+                }
+            }
+        }
+        
+        //Keyboard
         // Move Left
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            // Modify position
-            transform.position += new Vector3(-1, 0, 0);
-       
-            // See if valid
-            if (isValidGridPos())
-                // It's valid. Update grid.
-                updateGrid();
-            else
-                // It's not valid. revert.
-                transform.position += new Vector3(1, 0, 0);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            moveLeft();
         }
 
         // Move Right
         else if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            // Modify position
-            transform.position += new Vector3(1, 0, 0);
-       
-            // See if valid
-            if (isValidGridPos())
-                // It's valid. Update grid.
-                updateGrid();
-            else
-                // It's not valid. revert.
-                transform.position += new Vector3(-1, 0, 0);
+            moveRight();
         }
 
         // Rotate
         else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.Rotate(0, 0, -90);
-       
-            // See if valid
-            if (isValidGridPos())
-                // It's valid. Update grid.
-                updateGrid();
-            else
-                // It's not valid. revert.
-                transform.Rotate(0, 0, 90);
+            rotate();
         }
 
         // Move Downwards and Fall
         else if (Input.GetKeyDown(KeyCode.DownArrow) ||
                  Time.time - lastFall >= 1) {
-            // Modify position
-            transform.position += new Vector3(0, -1, 0);
-
-            // See if valid
-            if (isValidGridPos()) {
-                // It's valid. Update grid.
-                updateGrid();
-            } else {
-                // It's not valid. revert.
-                transform.position += new Vector3(0, 1, 0);
-
-                // Clear filled horizontal lines
-                Playfield.deleteFullRows();
-
-                // Spawn next Group
-                FindObjectOfType<Spawner>().spawnNext();
-
-                // Disable script
-                enabled = false;
-            }
-
-            lastFall = Time.time;
+            fall();
         }
 
     }
+
+    void TouchBegan(Touch touch)
+    {
+        initialPosition = touch.position;
+        touchBeginTime = Time.time;
+    }
+    void HandleMove(Touch touch)
+    {
+        Vector2 diff = touch.position - initialPosition;
+        if (Math.Abs(diff.x) > Math.Abs(diff.y))
+        {
+            if (diff.x > 0)
+            {
+                moveRight();
+            }
+            else if(diff.x < 0)
+            {
+                moveLeft();
+            }
+        }
+        else
+        {
+            if (diff.y > 0)
+            {
+                rotate();
+            }
+            else if (diff.y < 0)
+            {
+                fall();
+            }
+        }
+    }
+    
+    void moveLeft()
+    {
+        // Modify position
+        transform.position += new Vector3(-1, 0, 0);
+       
+        // See if valid
+        if (isValidGridPos())
+            // It's valid. Update grid.
+            updateGrid();
+        else
+            // It's not valid. revert.
+            transform.position += new Vector3(1, 0, 0);
+    }
+
+    void moveRight()
+    {
+        // Modify position
+        transform.position += new Vector3(1, 0, 0);
+       
+        // See if valid
+        if (isValidGridPos())
+            // It's valid. Update grid.
+            updateGrid();
+        else
+            // It's not valid. revert.
+            transform.position += new Vector3(-1, 0, 0);
+    }
+
+    void rotate()
+    {
+        transform.Rotate(0, 0, -90);
+       
+        // See if valid
+        if (isValidGridPos())
+            // It's valid. Update grid.
+            updateGrid();
+        else
+            // It's not valid. revert.
+            transform.Rotate(0, 0, 90);
+    }
+
+    void fall()
+    {
+        // Modify position
+        transform.position += new Vector3(0, -1, 0);
+
+        // See if valid
+        if (isValidGridPos()) {
+            // It's valid. Update grid.
+            updateGrid();
+        } else {
+            // It's not valid. revert.
+            transform.position += new Vector3(0, 1, 0);
+
+            // Clear filled horizontal lines
+            Playfield.deleteFullRows();
+
+            // Spawn next Group
+            FindObjectOfType<Spawner>().spawnNext();
+
+            // Disable script
+            enabled = false;
+        }
+
+        lastFall = Time.time;
+    }
+    
 }
