8e752b83a464450cc41281b9711da3b622b90649
hathas07 <35289971+hathas07@users.noreply.github.com>
2021-10-28 10:52:49 +0200

    Je me débarasse de mes merdes
    ceci est un push contenant mes merdes mais également le début de la reconnaissance de gesture

--- Tretriss/Assets/TetrisBlock.cs
+++ Tretriss/Assets/TetrisBlock.cs
@@ -19,6 +19,7 @@ public class TetrisBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
