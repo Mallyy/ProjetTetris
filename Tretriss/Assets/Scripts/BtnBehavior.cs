92df79d495c46056655b241b969a024aabc2f4a2
Mallyy <48942370+Mallyy@users.noreply.github.com>
2021-10-28 15:07:24 +0200

    add link commencer vers jeu

--- Tretriss/Assets/Scripts/BtnBehavior.cs
+++ Tretriss/Assets/Scripts/BtnBehavior.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BtnBehavior : MonoBehaviour
 {
+
+	public void startGame()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
     	public void quitGame()
     	{
     		Debug.Log("QUIT!");
