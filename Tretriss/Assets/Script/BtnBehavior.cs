e4b83c653281e8f3ff0110cae845f1acc31b1a8b
Vinciane <qlqqlc@outlook.fr>
2021-10-28 11:57:06 +0200

    passage au model tel graphiquement

--- Tretriss/Assets/Script/BtnBehavior.cs
+++ Tretriss/Assets/Script/BtnBehavior.cs
@@ -4,24 +4,9 @@ using UnityEngine;
 
 public class BtnBehavior : MonoBehaviour
 {
-    public void commencer()
-    	{
-    		
-    	}
-    	
-    	public void score()
-    	{
-    		
-    	}
-    	
     	public void quitGame()
     	{
     		Debug.Log("QUIT!");
     		Application.Quit();
-    	}
-    	
-    	public void credit()
-    	{
-    	
     	}
 }
